0a583dfff5ff85e99bace65733583e322ea03d71
Reza <3228126+i2@users.noreply.github.com>
2022-06-20 11:46:03 -0300

    2022/06/20 - 11:46:03

--- init.py
+++ init.py
@@ -6,7 +6,7 @@ import subprocess
 args = list(sys.argv)
 
 dname = str(subprocess.check_output(['sh', './src/distros/detect.sh']))
-return dname.replace("b'","").replace('"',"").replace("\\n'","")
+dname.replace("b'","").replace('"',"").replace("\\n'","")
 
 def main():
     if 'debian' in dname:
