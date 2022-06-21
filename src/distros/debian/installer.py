3f8c19eb4c2586dcc30c348823951acd25a20c09
Reza <3228126+i2@users.noreply.github.com>
2022-06-21 20:11:14 -0300

    2022/06/21 - 20:11:14

--- src/distros/debian/installer.py
+++ src/distros/debian/installer.py
@@ -122,41 +122,29 @@ def main(args):
     else:
         efi = False
 
-### STEP 1
-
     # Partitioning
     os.system("find $HOME -maxdepth 1 -type f -iname '.*shrc' -exec sh -c 'echo export LC_ALL=C LANGUAGE=C LANG=C >> $1' -- {} \;") # Perl complains if not set
     os.system("sudo apt-get remove -y --purge man-db") # make installs faster (because of trigger man-db bug)
-    os.system("sudo apt-get update")
+    os.system("sudo apt-get update -y")
     os.system("sudo apt-get autoremove -y")
     os.system("sudo apt-get install -y parted btrfs-progs dosfstools ntp")
     os.system("sudo parted --align minimal --script /dev/sda mklabel gpt unit MiB mkpart ESP fat32 0% 256 set 1 boot on mkpart primary ext4 256 100%")
     os.system("sudo /usr/sbin/mkfs.vfat -F32 -n EFI /dev/sda1")
     os.system(f"sudo /usr/sbin/mkfs.btrfs -L LINUX -f {args[1]}")
 
-    # Sync time in the live iso (optional)
-    print("echo 'Installing ntp. It will pause 30s. Sometimes it's needed to restart ntp service to have time sync again'")
-    os.system("sudo systemctl enable --now ntp && sleep 30s && ntpq -p")
-    #os.system("sudo apt-get update")
-
     # Mount and create necessary sub-volumes and directories
     os.system(f"sudo mount {args[1]} /mnt")
-
     for btrdir in btrdirs:
         os.system(f"sudo btrfs sub create /mnt/{btrdir}")
-
     os.system("sudo umount /mnt")
     os.system(f"sudo mount {args[1]} -o subvol=@,compress=zstd,noatime /mnt")
-
     for mntdir in mntdirs:
         os.system(f"sudo mkdir /mnt/{mntdir}")
         os.system(f"sudo mount {args[1]} -o subvol={btrdirs[mntdirs.index(mntdir)]},compress=zstd,noatime /mnt/{mntdir}")
-
     for i in ("tmp", "root"):
         os.system(f"mkdir -p /mnt/{i}")
     for i in ("ast", "boot", "etc", "root", "rootfs", "tmp", "var"):
         os.system(f"mkdir -p /mnt/.snapshots/{i}")
-
     if efi:
         os.system("sudo mkdir /mnt/boot/efi")
         os.system(f"sudo mount {args[3]} /mnt/boot/efi")
@@ -167,8 +155,6 @@ def main(args):
     os.system("find /mnt/root/ -maxdepth 1 -type f -iname '.*shrc' -exec sh -c 'echo export LC_ALL=C | sudo tee -a $1' -- {} \;")
     os.system("echo 'setw -g mode-keys vi' | tee -a $HOME/.tmux.conf")
 
-### STEP 2
-
     # Bootstrap
     os.system("sudo apt-get install -y debootstrap")
     os.system(f"sudo debootstrap --arch {ARCH} {RELEASE} /mnt http://ftp.debian.org/debian")
@@ -178,17 +164,15 @@ def main(args):
     os.system(f"sudo chroot /mnt apt-get install -y linux-image-{ARCH}")
 
     # Install anytree and necessary packages in chroot
+    os.system("sudo systemctl enable --now ntp && sleep 30s && ntpq -p") # Sync time in the live iso
     os.system(f"echo 'deb [trusted=yes] http://www.deb-multimedia.org {RELEASE} main' | sudo tee -a /mnt/etc/apt/sources.list.d/multimedia.list >/dev/null")
-    os.system("sudo chroot /mnt apt-get update -oAcquire::AllowInsecureRepositories=true")
+    os.system("sudo chroot /mnt apt-get update -y -oAcquire::AllowInsecureRepositories=true")
     os.system("sudo chroot /mnt apt-get install -y deb-multimedia-keyring --allow-unauthenticated")
-    ##### os.system("sudo chmod -R 1777 /mnt/tmp") #REZA this might need to be commented out if no error
     os.system("sudo chroot /mnt apt-get install -y python3-anytree network-manager btrfs-progs dhcpcd5 locales sudo")
     if efi:
         os.system("sudo chroot /mnt apt-get install -y grub-efi")
     else:
         os.system("sudo chroot /mnt apt-get install -y grub-pc")
-
-### STEP 3
 
     os.system(f"echo 'UUID=\"{to_uuid(args[1])}\" / btrfs subvol=@,compress=zstd,noatime,ro 0 0' | sudo tee /mnt/etc/fstab")
 
@@ -201,7 +185,7 @@ def main(args):
     os.system("echo '/.snapshots/ast/root /root none bind 0 0' | sudo tee -a /mnt/etc/fstab")
     os.system("echo '/.snapshots/ast/tmp /tmp none bind 0 0' | sudo tee -a /mnt/etc/fstab")
 
-    astpart = to_uuid(args[1]) ###THIS will be removed when partitioning happens outside this installer
+    astpart = to_uuid(args[1])
 
     os.system("sudo mkdir -p /mnt/usr/share/ast/db")
     os.system("echo '0' | sudo tee /mnt/usr/share/ast/snap")
@@ -221,8 +205,6 @@ def main(args):
 
     os.system(f"sudo chroot /mnt ln -sf {tz} /etc/localtime")
 
-### STEP 4
-
     os.system("sudo sed -i 's/^#en_US.UTF-8/en_US.UTF-8/g' /etc/locale.gen")
     os.system("sudo chroot /mnt locale-gen")
     os.system("sudo chroot /mnt hwclock --systohc")
@@ -239,8 +221,6 @@ def main(args):
     set_password("root")
 
     os.system("sudo chroot /mnt systemctl enable NetworkManager")
-
-### STEP 5
 
     os.system("echo {\\'name\\': \\'root\\', \\'children\\': [{\\'name\\': \\'0\\'}]} | sudo tee /mnt/.snapshots/ast/fstree")
 
@@ -260,7 +240,7 @@ def main(args):
     os.system("sudo btrfs sub create /mnt/.snapshots/var/var-tmp")
     os.system("sudo btrfs sub create /mnt/.snapshots/boot/boot-tmp")
     for i in ("dpkg", "systemd"):
-        os.system(f"sudo mkdir -p /mnt/.snapshots/var/var-tmp/lib/{i}")   # i didn't have sudo here before, so probbly wasn't creating these folders!
+        os.system(f"sudo mkdir -p /mnt/.snapshots/var/var-tmp/lib/{i}")
     os.system("sudo cp --reflink=auto -r /mnt/var/lib/dpkg/* /mnt/.snapshots/var/var-tmp/lib/dpkg/")
     os.system("sudo cp --reflink=auto -r /mnt/var/lib/systemd/* /mnt/.snapshots/var/var-tmp/lib/systemd/")
     os.system("sudo cp --reflink=auto -r /mnt/boot/* /mnt/.snapshots/boot/boot-tmp")
@@ -284,26 +264,18 @@ def main(args):
         os.system("sudo btrfs sub snap /mnt/.snapshots/rootfs/snapshot-0 /mnt/.snapshots/rootfs/snapshot-tmp")
         os.system("sudo chroot /mnt btrfs sub set-default /.snapshots/rootfs/snapshot-tmp")
 
-### STEP 6 (is in fact 3 line above but for aesthetics I put it here. Will be removed anyways!
-
     os.system("sudo cp -r /mnt/root/. /mnt/.snapshots/root/")
     os.system("sudo cp -r /mnt/tmp/. /mnt/.snapshots/tmp/")
     os.system("sudo rm -rf /mnt/root/*")
     os.system("sudo rm -rf /mnt/tmp/*")
-#    os.system("umount /mnt/var")
 
     if efi:
         os.system("sudo umount /mnt/boot/efi")
 
     os.system("sudo umount /mnt/boot")
-#    os.system("mkdir /mnt/.snapshots/var/var-tmp")
-#    os.system("mkdir /mnt/.snapshots/boot/boot-tmp")
-#    os.system(f"mount {args[1]} -o subvol=@var,compress=zstd,noatime /mnt/.snapshots/var/var-tmp")
     os.system(f"sudo mount {args[1]} -o subvol=@boot,compress=zstd,noatime /mnt/.snapshots/boot/boot-tmp")
-#    os.system("cp --reflink=auto -r /mnt/.snapshots/var/var-tmp/* /mnt/var")
-    os.system("sudo cp --reflink=auto -r /mnt/.snapshots/boot/boot-tmp/* /mnt/boot") #why umounted /mnt/boot and then do this?
+    os.system("sudo cp --reflink=auto -r /mnt/.snapshots/boot/boot-tmp/* /mnt/boot")
     os.system("sudo umount /mnt/etc")
-#    os.system("mkdir /mnt/.snapshots/etc/etc-tmp")
     os.system(f"sudo mount {args[1]} -o subvol=@etc,compress=zstd,noatime /mnt/.snapshots/etc/etc-tmp")
     os.system("sudo cp --reflink=auto -r /mnt/.snapshots/etc/etc-tmp/* /mnt/etc")
 
@@ -316,13 +288,12 @@ def main(args):
         os.system("sudo cp --reflink=auto -r /mnt/.snapshots/var/var-0/* /mnt/.snapshots/rootfs/snapshot-tmp/var")
         os.system("sudo cp --reflink=auto -r /mnt/.snapshots/boot/boot-0/* /mnt/.snapshots/rootfs/snapshot-tmp/boot")
 
-###  STEP 7
-
-#    os.system("sudo umount -R /mnt")   #gives error /mnt target busy --- maybe I should use: sudo umount -R /mnt/*
-#    os.system(f"sudo mount {args[1]} /mnt")
-###    os.system("sudo btrfs sub del /mnt/@") # it gives an error could not statfs: No such file or directory (could not statfs, no such file or directory)
-#    os.system("sudo umount -R /mnt")
-#    clear()
-#    print("Installation complete")
-#    print("You can reboot now :)")
+    # Unmount everything
+    os.system("sudo umount -R /mnt")
+    os.system(f"sudo mount {args[1]} -o subvolid=5 /mnt")
+    os.system("sudo btrfs sub del /mnt/@")
+    os.system("sudo umount -R /mnt")
+    clear()
+    print("Installation complete")
+    print("You can reboot now :)")
 
