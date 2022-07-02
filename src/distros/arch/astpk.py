fdc798a01d5c5a3d7598391465b0c286ab9ec1c6
Reza <3228126+i2@users.noreply.github.com>
2022-07-02 16:17:49 -0300

    Commit before removing cruft from installer.py 2022/07/02 - 16:17:49

--- src/distros/arch/astpk.py
+++ src/distros/arch/astpk.py
@@ -55,124 +55,14 @@ def switch_distro():
                 for row in input_file:
                     if row["DISTRO"] == next_distro:
                         #os.system(f'efibootmgr --bootnext {row["BootOrder"]}')
-                        bo = subprocess.check_output("efibootmgr | grep BootOrder | awk '{print $1}'", shell=True).decode('utf-8').strip()
-                        temp = bo.replace(f'{row["BootOrder"]},', "")
-                        newbo = f"{row['BootOrder']},{temp}"
-                        os.system(f'efibootmgr --bootorder {newbo}')
+                        boot_order = subprocess.check_output("efibootmgr | grep BootOrder | awk '{print $1}'", shell=True).decode('utf-8').strip()
+                        temp = boot_order.replace(f'{row["BootOrder"]},', "")
+                        new_boot_order = f"{row['BootOrder']},{temp}"
+                        os.system(f'efibootmgr --bootorder {new_boot_order}')
             break
         else:
             print("Invalid distro!")
             continue
-
-def switch_distro_OLDEST(p, next_distro):
-    # Move grub and efi
-    tmp_efi= subprocess.check_output("cat /dev/urandom | od -x | tr -d ' ' | head -n 1", shell=True).decode('utf-8').strip()
-    os.system(f"mkdir /tmp/{tmp_efi}")
-    os.system(f"mount /dev/{p} /tmp/{tmp_efi}")
-    os.system(f"mv /tmp/{tmp_efi}/boot/efi/EFI/ashos /tmp/{tmp_efi}/boot/efi/EFI/ashos{distro}")
-    os.system(f"mv /tmp/{tmp_efi}/boot/efi/EFI/ashos_{next_distro} /tmp/{tmp_efi}/boot/efi/EFI/ashos")
-    os.system(f"umount /tmp/{tmp_efi}")
-    tmp_efi= subprocess.check_output("cat /dev/urandom | od -x | tr -d ' ' | head -n 1", shell=True).decode('utf-8').strip()
-    os.system(f"mkdir /tmp/{tmp_efi}")
-    os.system(f"mount /dev/{p} /tmp/{tmp_efi}")
-    os.system(f"mv /mnt/boot/grub /mnt/boot/grub{distro}")
-    os.system(f"mv /mnt/boot/grub_{next_distro} /mnt/boot/grub")
-
-def switch_distro_OLDER(p):
-    # Move grub and efi
-    # Look in /mnt/boot/efi/EFI/ for folders with 'ashos' in their name, present them as options to user
-###    next_distro_options = subprocess.check_output("find /mnt/boot/efi/EFI/ -mindepth 1 -maxdepth 1 -type \
-###        d -name '*ashos*' -o -exec basename {} \;", shell=True).decode('utf-8').strip()
-    while True:
-        print("Type the name of a distro to switch to: (type 'list' to list them)")
-        next_distro = input("> ")
-        if next_distro == "q":
-            break
-        elif next_distro == "list":
-            os.system("ls /boot/efi/EFI/ | grep -i 'ashos' | sed 's/_ashos//' | less")
-        elif os.path.exists(f"/boot/efi/EFI/{next_distro}_ashos"):
-            os.system(f"mv /boot/grub /boot/grub{distro}_ashos")
-            os.system(f"mv /boot/grub{next_distro}_ashos /boot/grub")
-            os.system(f"mv /boot/efi/EFI/{distro} /boot/efi/EFI/{distro}_ashos")
-            os.system(f"mv /boot/efi/EFI/{next_distro}_ashos /boot/efi/EFI/{next_distro}")
-            os.system(f"echo {next_distro} >> /boot/efi/EFI/default.txt")
-            break
-        else:
-            print("Invalid distro!")
-            continue
-
-def switch_distro_OLX(next_distro):
-    import csv
-    
-    with open('/home/me/Downloads/meeeeeeeeeeeee/awk-test.txt', 'r') as f:
-        input_file = csv.DictReader(f, delimiter=',', quoting=csv.QUOTE_NONE)
-        for row in input_file:
-            if row["DISTRO"] == next_distro:
-                return row["BootOrder"]
-
-def choose_distro_to_switch_OLDER():
-    import csv
-    with open('/home/me/Downloads/meeeeeeeeeeeee/awk-test.txt','r') as f:
-    data = list( csv.reader(f, delimiter ="=") )
-    while True:
-        print("Type the name of a distro to switch to: (type 'list' to list them)")
-        next_distro = input("> ")
-        if next_distro == "q":
-            break
-        elif next_distro == "list":
-            os.system("cat /boot/efi/EFI/map.txt | awk 'BEGIN { FS = "'"'" === "'"'" } ; { print $1 }' | less")
-        elif next_distro in subprocess.check_output("cat /boot/efi/EFI/map.txt | awk 'BEGIN { FS = "'"'" === "'"'" } ; { print $1 }'", shell=True).decode('utf-8').strip():
-            return subprocess.check_output(f"cat /boot/efi/EFI/map.txt | grep {next_distro}' | awk '{{print $(NF)}}'", shell=True)
-            break
-        else:
-            print("Invalid distro!")
-            continue
-
-def choose_distro_to_switch_OLDEST():
-    while True:
-        print("Type the name of a distro to switch to: (type 'list' to list them)")
-        next_distro = input("> ")
-        if next_distro == "q":
-            break
-        elif next_distro == "list":
-            os.system("cat /boot/efi/EFI/map.txt | awk 'BEGIN { FS = "'"'" === "'"'" } ; { print $1 }' | less")
-        elif next_distro in subprocess.check_output("cat /boot/efi/EFI/map.txt | awk 'BEGIN { FS = "'"'" === "'"'" } ; { print $1 }'", shell=True).decode('utf-8').strip():
-            return subprocess.check_output(f"cat /boot/efi/EFI/map.txt | grep {next_distro}' | awk '{{print $(NF)}}'", shell=True)
-            break
-        else:
-            print("Invalid distro!")
-            continue
-
-    os.system(f"mv /tmp/{tmp_efi}/boot/efi/EFI/ashos /tmp/{tmp_efi}/boot/efi/EFI/ashos{distro}")
-    os.system(f"mv /tmp/{tmp_efi}/boot/efi/EFI/ashos_{next_distro} /tmp/{tmp_efi}/boot/efi/EFI/ashos")
-    os.system(f"umount /tmp/{tmp_efi}")
-    tmp_efi= subprocess.check_output("cat /dev/urandom | od -x | tr -d ' ' | head -n 1", shell=True).decode('utf-8').strip()
-    os.system(f"mkdir /tmp/{tmp_efi}")
-    os.system(f"mount /dev/{p} /tmp/{tmp_efi}")
-    os.system(f"mv /mnt/boot/grub /mnt/boot/grub{distro}")
-    os.system(f"mv /mnt/boot/grub_{next_distro} /mnt/boot/grub")
-
-### #   Rename grub in sda1 from default ashos to pdistro_ashos
-### def rename_ashos_grub(p, pd):
-###     tmp_efi= subprocess.check_output("cat /dev/urandom | od -x | tr -d ' ' | head -n 1", shell=True).decode('utf-8').strip()
-###     os.system(f"mkdir /tmp/{tmp_efi}")
-###     os.system(f"mount /dev/{p} /tmp/{tmp_efi}")
-###     #os.system(f"mkdir /tmp/{tmp_efi}/ashos")
-###     #os.system(f"cp -r /tmp/{tmp_efi}/EFI/${d}_ashos/")
-###     #os.system(f"mkdir /tmp/{tmp_efi}/ashos_{pd}") #THIS IS REDUNDANT as cp -a creates the dest folder even if doesn't exist
-###     #os.system(f"cp -a /tmp/{tmp_efi}/EFI/ashos/. /tmp/{tmp_efi}/EFI/ashos_{pd}/")
-###     os.system(f"cp -a /tmp/{tmp_efi}/EFI/ashos /tmp/{tmp_efi}/EFI/ashos_{pd}")
-###     os.system(f"umount /tmp/{tmp_efi}")
-###     #os.system(f"rm -rf /tmp/{tmp_efi}") ##if successful umount ( check result of 'echo $?' )
-
-############# BOTH WHEN INSTALLING ASHOS / UPDATING AKA DEPLOYING FROM AST, CREATE A BACKUP OF FILES
-############# sudo mv /tmp/{tmp_efi}/EFI/ashos/grub.cfg /tmp/{tmp_efi}/EFI/ashos/grub.cfg{DISTRO}
-############# sudo mv /tmp/{tmp_efi}/EFI/ashos/grubx64.efi /tmp/{tmp_efi}/EFI/ashos/grubx64.efi{DISTRO}
-############# sudo mv /tmp/{tmp_efi}/EFI/ashos/grub{NEXTDISTRO}.cfg /tmp/{tmp_efi}/EFI/ashos/grub.cfg
-############# sudo mv /tmp/{tmp_efi}/EFI/ashos/grubx64.efi{NEXTDISTRO} /tmp/{tmp_efi}/EFI/ashos/grubx64.efi
-
-#IF THE GRUB IN INSTALLER OF A DISTRO DIDN'T CREATE {THAT "REFERENCE" grub.cfg} in /dev/sda1 (for example arch doesn't -- maybe it creates it in /dev/sda2 ???? double check!)
-# CREATE IT FROM MY CACHE (either store in astpk.py or installer.py as a paragraph) and copy it over to /tmp/{tmp_efi}/EFI/ashos/ as grub.cfg
 
 def detect_previous_distro(p):
     tmp_sda2 = subprocess.check_output("cat /dev/urandom | od -x | tr -d ' ' | head -n 1", shell=True).decode('utf-8').strip()
