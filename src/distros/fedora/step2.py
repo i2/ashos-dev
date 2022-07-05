fd206822ec8f7002efa120e2f8082a84821b8432
Reza <3228126+i2@users.noreply.github.com>
2022-07-05 12:29:23 -0300

    2022/07/05 - 12:29:23

--- src/distros/fedora/step2.py
+++ src/distros/fedora/step2.py
@@ -112,6 +112,7 @@ def main(args, distro):
     os.system(f"dnf makecache --refresh --releasever={RELEASE} -c ./src/distros/fedora/base.repo")
     
     for i in ("/dev", "/dev/pts", "/proc", "/run", "/sys", "/sys/firmware/efi/efivars"):  ### REZA In debian, these mount-points operations go 'after' debootstrapping and there is no complaint! In fedora, if so, dnf would complain /dev is not mounted!
+        os.system("mkdir -p /mnt{i}")
         os.system(f"mount -B {i} /mnt{i}") # Mount-points needed for chrooting
     
     os.system(f"dnf -c ./src/distros/fedora/base.repo --installroot=/mnt install dnf -y --releasever={RELEASE}")  #### removed basearch as it was giving unrecognized arguments error!
