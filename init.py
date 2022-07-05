582f9a225595217c5be7c1abcfac66c4cefcb5be
Reza <3228126+i2@users.noreply.github.com>
2022-07-04 23:15:17 -0300

    2022/07/04 - 23:15:17

--- init.py
+++ init.py
@@ -4,14 +4,18 @@ import sys
 import subprocess
 
 args = list(sys.argv)
-distro = subprocess.check_output(['sh', './src/detect_os.sh']).decode('utf-8').replace('"',"").strip()
+
+if args[3]:
+    distro = args[3]
+else:
+    distro = subprocess.check_output(['sh', './src/detect_os.sh']).decode('utf-8').replace('"',"").strip()
 
 if 'debian' in distro:
     from src.distros.debian import installer
 elif 'arch' in distro:
     from src.distros.arch import installer
-    #from src.distros.arch import installer_refind
+elif 'fedora' in distro:
+    from src.distros.fedora import step1
 
 installer.main(args, distro)
-#installer_refind.main(args, distro)
 
