d6e74eaba110f9febae18629f2e271e247369a20
Reza <3228126+i2@users.noreply.github.com>
2022-07-08 10:02:10 -0300

    2022/07/08 - 10:02:09

--- src/prep/parted-efi-example.sh
+++ src/prep/parted-efi-example.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-sudo parted --align minimal --script $1 mklabel gpt unit MiB mkpart ESP fat32 0% 256 set 1 boot on mkpart primary ext4 256 20% mkpart primary ext4 20% 40%
+sudo parted --align minimal --script $1 mklabel gpt unit MiB mkpart ESP fat32 0% 256 \
+            set 1 boot on mkpart primary ext4 256 20% mkpart primary ext4 20% 40%
 sudo /usr/sbin/mkfs.vfat -F32 -n EFI ${1}1
 
