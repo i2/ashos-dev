d4a7224cf52f327a4d75e1955c3b5f30ff3db5bd
Reza <3228126+i2@users.noreply.github.com>
2022-07-11 21:35:01 -0300

    IMPORTANT: Incorporate CJ commit july11 removing .snapshot specific var folder in Arch - will do for other distros when test finished continued

--- src/prep/arch-prep.sh
+++ src/prep/arch-prep.sh
@@ -1,13 +1,13 @@
-sed -e '/^SigLevel/ s/^#*/SigLevel = Never\n#/' -i mee.txt
-sed -e '/^LocalFileSigLevel/ s/^#*/#/' -i mee.txt
+sed -e '/^SigLevel/ s/^#*/SigLevel = Never\n#/' -i /etc/pacman.conf
+sed -e '/^LocalFileSigLevel/ s/^#*/#/' -i /etc/pacman.conf
 pacman -Syy --noconfirm archlinux-keyring
 pacman -S --noconfirm git
 echo "export LC_ALL=C LC_CTYPE=C LANGUAGE=C" | tee -a $HOME/.zshrc
-echo "alias p='curl -F "'"sprunge=<-"'" sprunge.us" | tee -a $HOME/.zshrc
+echo "alias p='curl -F "'"sprunge=<-"'" sprunge.us'" | tee -a $HOME/.zshrc
 echo "setw -g mode-keys vi" | tee -a $HOME/.tmux.conf
 git clone http://github.com/i2/ashos-dev
 cd ashos-dev
 git checkout debian
-#/bin/sh ./src/part-efi-example.sh /dev/sda
+/bin/sh ./src/part-efi-example.sh /dev/sda
 #python3 init.py /dev/sda2 /dev/sda /dev/sda1
 
--- src/prep/debian-prep.sh
+++ src/prep/debian-prep.sh
@@ -1,10 +1,10 @@
 sudo apt-get -y update && sudo apt-get -y install git tmux parted btrfs-progs dosfstools
 echo "export LC_ALL=C LC_CTYPE=C LANGUAGE=C" | tee -a $HOME/.bashrc
-echo "alias p='curl -F "'"sprunge=<-"'" sprunge.us" | tee -a $HOME/.bashrc
+echo "alias p='curl -F "'"sprunge=<-"'" sprunge.us'" | tee -a $HOME/.bashrc
 echo "setw -g mode-keys vi" | tee -a $HOME/.tmux.conf
 git clone http://github.com/i2/ashos-dev
 cd ashos-dev
 git checkout debian
-#/bin/bash ./src/part-efi-example.sh /dev/sda
+/bin/bash ./src/part-efi-example.sh /dev/sda
 #sudo python3 init.py /dev/sda2 /dev/sda /dev/sda1
 
