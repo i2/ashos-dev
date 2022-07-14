8bfd4dade508734770c949c50594dc6f436cfa3e
Reza <3228126+i2@users.noreply.github.com>
2022-07-14 16:00:29 -0300

    2022/07/14 - 16:00:29

---
+++ src/distros/fedora/step1.py
@@ -0,0 +1,154 @@
+#!/usr/bin/python3
+
+# might need to append /bin/sh or /bin/bash to chroot commands, as arch iso live cd use zsh and choroot environment is bash
+
+import os
+import subprocess
+import sys
+
+def clear():
+    os.system("#clear")
+
+def to_uuid(part):
+    return subprocess.check_output(f"/usr/sbin/blkid -s UUID -o value {part}", shell=True).decode('utf-8').strip()
+
+#   This function returns a tuple: (1. choice whether partitioning and formatting should happen
+#   2. Underscore plus name of distro if it should be appended to sub-volume names
+def get_multiboot(dist):
+    clear()
+    while True:
+        print("Please choose one of the following:\n1. Single OS installation\n2. Initiate a multi-boot ashos setup\n3. Adding to an already installed ashos")
+        print("Please be aware choosing option 1 and 2 will wipe {args[1]}")
+        i = input("> ")
+        if i == "1":
+            return i,""
+            break
+        elif i == "2":
+            return i,f"_{dist}"
+            break
+        elif i == "3":
+            return i,f"_{dist}"
+            break
+        else:
+            print("Invalid choice!")
+            continue
+
+def get_hostname():
+    clear()
+    while True:
+        print("Enter hostname:")
+        hostname = input("> ")
+        if hostname:
+            print("Happy with your hostname (y/n)?")
+            reply = input("> ")
+            if reply.casefold() == "y":
+                break
+            else:
+                continue
+    return hostname
+
+def get_timezone():
+    clear()
+    while True:
+        print("Select a timezone (type list to list):")
+        zone = input("> ")
+        if zone == "list":
+            os.system("ls /usr/share/zoneinfo | less")
+        elif os.path.isfile(f"/usr/share/zoneinfo/{zone}"):
+            return str(f"/usr/share/zoneinfo/{zone}")
+        else:
+            print("Invalid Timezone!")
+            continue
+
+def get_username():
+    clear()
+    while True:
+        print("Enter username (all lowercase, max 8 letters)")
+        username = input("> ")
+        if username:
+            print("Happy with your username (y/n)?")
+            reply = input("> ")
+            if reply.casefold() == "y":
+                break
+            else:
+                continue
+    return username
+
+def create_user(u):
+    os.system(f"chroot /mnt /usr/sbin/useradd -m -G wheel -s /bin/bash {u}")
+    os.system("echo '%wheel ALL=(ALL:ALL) ALL' | tee -a /mnt/etc/sudoers")
+    os.system(f"echo 'export XDG_RUNTIME_DIR=\"/run/user/1000\"' | tee -a /mnt/home/{u}/.bashrc")
+
+def set_password(u):
+    clear()
+    while True:
+        print(f"Setting a password for '{u}':")
+        os.system(f"chroot /mnt passwd {u}")
+        print("Was your password set properly (y/n)?")
+        reply = input("> ")
+        if reply.casefold() == "y":
+            break
+        else:
+            continue
+
+def main(args, distro):
+    print("Welcome to the astOS installer!\n\n\n\n\n")
+    choice, distro_suffix = get_multiboot(distro)
+
+#   Define variables
+    ARCH="x86_64"
+    RELEASE="rawhide"
+    #astpart = to_uuid(args[1])
+    btrdirs = [f"@{distro_suffix}",f"@.snapshots{distro_suffix}",f"@home{distro_suffix}",f"@var{distro_suffix}",f"@etc{distro_suffix}",f"@boot{distro_suffix}"]
+    mntdirs = ["",".snapshots","home","var","etc","boot"]
+    if os.path.exists("/sys/firmware/efi"):
+        efi = True
+    else:
+        efi = False
+    #packages = "passwd which grub2-efi-x64-modules shim-x64 btrfs-progs python-anytree sudo tmux neovim NetworkManager dhcpcd"
+    packages = "passwd which grub2-efi-x64-modules shim-x64 btrfs-progs python-anytree sudo neovim"
+
+    tz = get_timezone()
+    hostname = get_hostname()
+
+#   Partition and format
+    if choice != "3":
+        os.system(f"/usr/sbin/mkfs.vfat -F32 -n EFI {args[3]}") ### DELETE THIS LINE WHEN PRODUCTION READY
+        os.system(f"/usr/sbin/mkfs.btrfs -L LINUX -f {args[1]}")
+    os.system("pacman -Syy --noconfirm archlinux-keyring dnf")
+
+    astpart = to_uuid(args[1]) ### DELETE THIS LINE WHEN PRODUCTION READY
+
+#   Mount and create necessary sub-volumes and directories
+    if choice != "3":
+        os.system(f"mount {args[1]} /mnt")
+    else:
+        os.system(f"mount -o subvolid=5 {args[1]} /mnt")
+    for btrdir in btrdirs:
+        os.system(f"btrfs sub create /mnt/{btrdir}")
+    os.system("umount /mnt")
+    for mntdir in mntdirs:
+        os.system(f"mkdir -p /mnt/{mntdir}") # -p to ignore /mnt exists complaint
+        os.system(f"mount {args[1]} -o subvol={btrdirs[mntdirs.index(mntdir)]},compress=zstd,noatime /mnt/{mntdir}")
+    for i in ("tmp", "root"):
+        os.system(f"mkdir -p /mnt/{i}")
+    for i in ("ast", "boot", "etc", "root", "rootfs", "tmp", "var"):
+        os.system(f"mkdir -p /mnt/.snapshots/{i}")
+    if efi:
+        os.system("mkdir /mnt/boot/efi")
+        os.system(f"mount {args[3]} /mnt/boot/efi")
+
+#   Modify shell profile for debug purposes in live iso (optional temporary)
+    #os.system('echo "alias paste='"'"'curl -F "'"'"'"sprunge=<-"'"'"'" http://sprunge.us'"'"' " | tee -a $HOME/.*shrc')
+    #os.system("shopt -s nullglob && echo 'export LC_ALL=C' | sudo tee -a /mnt/root/.*shrc")
+    #os.system("find /mnt/root/ -maxdepth 1 -type f -iname '.*shrc' -exec sh -c 'echo export LC_ALL=C | sudo tee -a $1' -- {} \;")
+    #os.system("echo -e 'setw -g mode-keys vi\nset -g history-limit 999999' >> $HOME/.tmux.conf")
+
+#   Pacstrap then install anytree and necessary packages in chroot
+    #os.system("pacstrap /mnt base linux linux-firmware neovim python3 python-anytree bash dhcpcd arch-install-scripts btrfs-progs networkmanager grub sudo tmux") # os-prober
+    os.system(f"dnf makecache --refresh --releasever={RELEASE} -c ./src/distros/fedora/base.repo")
+    #os.system("pacstrap /mnt base linux neovim python3 python-anytree arch-install-scripts btrfs-progs grub sudo tmux")
+    for i in ("/dev", "/dev/pts", "/proc", "/run", "/sys", "/sys/firmware/efi/efivars"):  ### REZA In debian, these mount-points operations go 'after' debootstrapping and there is no complaint! In fedora, if so, dnf would complain /dev is not mounted!
+        os.system(f"mkdir -p /mnt{i}")
+        os.system(f"mount -B {i} /mnt{i}") # Mount-points needed for chrooting
+    os.system(f"dnf -c ./src/distros/fedora/base.repo --installroot=/mnt install dnf -y --releasever={RELEASE} --basearch={ARCH}")  #### removed basearch as it was giving unrecognized arguments error!
