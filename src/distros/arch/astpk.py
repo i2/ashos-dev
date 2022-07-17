7584fb2029fe3e8027d0be0bc646d776b0800307
Reza <3228126+i2@users.noreply.github.com>
2022-07-16 23:08:25 -0300

    2022/07/16 - 23:08:25

--- src/distros/arch/astpk.py
+++ src/distros/arch/astpk.py
@@ -189,7 +189,7 @@ def get_tmp():
 
 #   Deploy snapshot
 def deploy(snapshot):
-    distro_suffix = get_distro_suffix() ### REVIEW_LATER ### JUST FOR FSTAB LINE BELOW, MIGHT NOT NEED IT IF I JUST REPLACE noatime,ro with noatime
+    #distro_suffix = get_distro_suffix() ### REVIEW_LATER ### JUST FOR FSTAB LINE BELOW, MIGHT NOT NEED IT IF I JUST REPLACE noatime,ro with noatime
     if not (os.path.exists(f"/.snapshots/rootfs/snapshot-{snapshot}")):
         print(f"F: cannot deploy as snapshot {snapshot} doesn't exist.")
     else:
@@ -211,7 +211,8 @@ def deploy(snapshot):
         os.system(f"cp --reflink=auto -r /.snapshots/etc/etc-{etc}/* /.snapshots/rootfs/snapshot-{tmp}/etc >/dev/null 2>&1")
         # If snapshot is mutable, modify '/' entry in fstab to 'rw' ### REVIEW_LATER
         if os.path.exists(f"/.snapshots/rootfs/snapshot-{snapshot}/usr/share/ast/mutable"):
-            os.system(f"sed -i '0,/@.snapshots{distro_suffix}/rootfs/snapshot-tmp/ s/noatime,ro/noatime/' /.snapshots/rootfs/snapshot-{tmp}/etc/fstab") ### REVIEW_LATER
+            #os.system(f"sed -i '0,/@.snapshots{distro_suffix}/rootfs/snapshot-tmp/ s/noatime,ro/noatime/' /.snapshots/rootfs/snapshot-{tmp}/etc/fstab") ### REVIEW_LATER
+            os.system(f"sed -i 's/noatime,ro/noatime/' /.snapshots/rootfs/snapshot-{tmp}/etc/fstab") ### REVIEW_LATER
         os.system(f"btrfs sub snap /var /.snapshots/rootfs/snapshot-{tmp}/var >/dev/null 2>&1")
         os.system(f"cp --reflink=auto -r /.snapshots/boot/boot-{etc}/* /.snapshots/rootfs/snapshot-{tmp}/boot >/dev/null 2>&1")
         os.system(f"echo '{snapshot}' > /.snapshots/rootfs/snapshot-{tmp}/usr/share/ast/snap")
@@ -667,7 +668,7 @@ def posttrans(snapshot):
     os.system(f"cp --reflink=auto -r /.snapshots/rootfs/snapshot-{tmp}/var/lib/systemd/* /var/lib/systemd >/dev/null 2>&1")
     os.system(f"btrfs sub snap {immutability} /.snapshots/rootfs/snapshot-chr{snapshot} /.snapshots/rootfs/snapshot-{snapshot} >/dev/null 2>&1")
     #os.system(f"mkdir -p /.snapshots/rootfs/snapshot-{i}/usr/share/ast") ### REVIEW_LATER MOST PROBABLY NOT NEEDED
-    os.system(f"touch /.snapshots/rootfs/snapshot-{snapshot}/usr/share/ast/mutable")
+    ###os.system(f"touch /.snapshots/rootfs/snapshot-{snapshot}/usr/share/ast/mutable") ###WHY DID I HAVE THIS UNCOMMENTED EARLIER?! WEIRD! MAYBE THERE WAS A REASON?!
     os.system(f"btrfs sub snap {immutability} /.snapshots/boot/boot-chr{snapshot} /.snapshots/boot/boot-{etc} >/dev/null 2>&1")
     unchr(snapshot)
 
