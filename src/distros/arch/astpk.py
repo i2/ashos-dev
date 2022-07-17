8e6159e45480b972e15f6b1ad8cfd3ce10601f2e
Reza <3228126+i2@users.noreply.github.com>
2022-07-16 21:24:36 -0300

    2022/07/16 - 21:24:36

--- src/distros/arch/astpk.py
+++ src/distros/arch/astpk.py
@@ -32,7 +32,7 @@ def immutability_disable(snapshot):
                 os.system(f"btrfs property set -ts /.snapshots/rootfs/snapshot-{snapshot} ro false")
                 os.system(f"touch /.snapshots/rootfs/snapshot-{snapshot}/usr/share/ast/mutable")
                 print(f"Snapshot {snapshot} successfully made mutable.")
-                write_desc(snapshot, " mutable ")
+                write_desc(snapshot, " MUTABLE ")
     else:
         print(f"F: Snapshot {snapshot} (base) should not be modified.")
 
@@ -48,7 +48,7 @@ def immutability_enable(snapshot):
                 os.system(f"rm /.snapshots/rootfs/snapshot-{snapshot}/usr/share/ast/mutable")
                 os.system(f"btrfs property set -ts /.snapshots/rootfs/snapshot-{snapshot} ro true")
                 print(f"Snapshot {snapshot} successfully made immutable.")
-                os.system(f"sed 's/ mutable //g' /.snapshots/ast/snapshots/{snapshot}-desc")
+                os.system(f"sed 's/ MUTABLE //g' /.snapshots/ast/snapshots/{snapshot}-desc")
     else:
         print(f"F: Snapshot {snapshot} (base) should not be modified.")
 
