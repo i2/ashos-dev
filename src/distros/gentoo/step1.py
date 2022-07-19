ffccd180ef8aa062dc40087bf8862f9d1eb17ac9
Reza <3228126+i2@users.noreply.github.com>
2022-07-19 01:55:18 -0300

    Initial gentoo work added 2022/07/19 - 01:55:18

---
+++ src/distros/gentoo/step1.py
@@ -0,0 +1,141 @@
+#!/usr/bin/python3
+
+import os
+import subprocess
+import sys ### REMOVE WHEN USING TRY CATCH
+
+def clear():
+    os.system("#clear")
+
+def to_uuid(part):
+    return subprocess.check_output(f"sudo blkid -s UUID -o value {part}", shell=True).decode('utf-8').strip()
+
+#   This function returns a tuple: (1. choice whether partitioning and formatting should happen
+#   2. Underscore plus name of distro if it should be appended to sub-volume names
+def get_multiboot(dist):
+    clear()
+    while True:
+        print("Please choose one of the following:\n1. Single OS installation\n2. Initiate a multi-boot ashos setup\n3. Adding to an already installed ashos")
+        print("Please be aware choosing option 1 and 2 will wipe {args[1]}")
+        i = input("> ")
+        if i == "1":
+            return i, ""
+            break
+        elif i == "2":
+            return i, f"_{dist}"
+            break
+        elif i == "3":
+            return i, f"_{dist}"
+            break
+        else:
+            print("Invalid choice!")
+            continue
+
+def get_hostname():
+    clear()
+    while True:
+        print("Enter hostname:")
+        hostname = input("> ")
+        if hostname:
+            print("Happy with your hostname (y/n)?")
+            reply = input("> ")
+            if reply.casefold() == "y":
+                break
+            else:
+                continue
+    return hostname
+
+def get_timezone():
+    clear()
+    while True:
+        print("Select a timezone (type list to list):")
+        zone = input("> ")
+        if zone == "list":
+            os.system("ls /usr/share/zoneinfo | less")
+        elif os.path.isfile(f"/usr/share/zoneinfo/{zone}"):
+            return str(f"/usr/share/zoneinfo/{zone}")
+        else:
+            print("Invalid Timezone!")
+            continue
+
+def get_username():
+    clear()
+    while True:
+        print("Enter username (all lowercase, max 8 letters)")
+        username = input("> ")
+        if username:
+            print("Happy with your username (y/n)?")
+            reply = input("> ")
+            if reply.casefold() == "y":
+                break
+            else:
+                continue
+    return username
+
+def create_user(u):
+    os.system(f"sudo chroot /mnt /usr/sbin/useradd -m -G wheel -s /bin/bash {u}")
+    os.system("echo '%wheel ALL=(ALL:ALL) ALL' | sudo tee -a /mnt/etc/sudoers")
+    os.system(f"echo 'export XDG_RUNTIME_DIR=\"/run/user/1000\"' | sudo tee -a /mnt/home/{u}/.bashrc")
+
+def set_password(u):
+    clear()
+    while True:
+        print(f"Setting a password for '{u}':")
+        os.system(f"sudo chroot /mnt passwd {u}")
+        print("Was your password set properly (y/n)?")
+        reply = input("> ")
+        if reply.casefold() == "y":
+            break
+        else:
+            continue
+
+def main(args, distro):
+    print("Welcome to the AshOS installer!\n\n\n\n\n")
+    choice, distro_suffix = get_multiboot(distro)
+
+#   Define variables
+    ARCH = "amd64"
+    astpart = to_uuid(args[1])
+    btrdirs = [f"@{distro_suffix}", f"@.snapshots{distro_suffix}", f"@boot{distro_suffix}", f"@etc{distro_suffix}", f"@home{distro_suffix}", f"@var{distro_suffix}"]
+    mntdirs = ["", ".snapshots", "boot", "etc", "home", "var"]
+    packages = "base linux linux-firmware nano python3 python-anytree bash dhcpcd arch-install-scripts btrfs-progs networkmanager grub sudo tmux os-prober"
+    if os.path.exists("/sys/firmware/efi"):
+        efi = True
+    else:
+        efi = False
+
+    tz = get_timezone()
+    hostname = get_hostname()
+
+#   Prep (format partition, etc.)
+    if choice != "3":
+        os.system(f"sudo /usr/sbin/mkfs.btrfs -L LINUX -f {args[1]}")
+    os.system("pacman -Syy --noconfirm archlinux-keyring")
+
+#   Mount and create necessary sub-volumes and directories
+    if choice != "3":
+        os.system(f"sudo mount {args[1]} /mnt")
+    else:
+        os.system(f"sudo mount -o subvolid=5 {args[1]} /mnt")
+    for btrdir in btrdirs:
+        os.system(f"sudo btrfs sub create /mnt/{btrdir}")
+    os.system("sudo umount /mnt")
+    for mntdir in mntdirs:
+        os.system(f"sudo mkdir -p /mnt/{mntdir}") # -p to ignore /mnt exists complaint
+        os.system(f"sudo mount {args[1]} -o subvol={btrdirs[mntdirs.index(mntdir)]},compress=zstd,noatime /mnt/{mntdir}")
+    for i in ("tmp", "root"):
+        os.system(f"mkdir -p /mnt/{i}")
+    for i in ("ast", "boot", "etc", "root", "rootfs", "tmp"): ### JULY11, 2022 removed "var" as it's not needed!
+        os.system(f"mkdir -p /mnt/.snapshots/{i}")
+    if efi:
+        os.system("sudo mkdir /mnt/boot/efi")
+        os.system(f"sudo mount {args[3]} /mnt/boot/efi")
+
+#   Bootstrap then install anytree and necessary packages in chroot
+    tmp = subprocess.check_output(f'curl -L http://distfiles.gentoo.org/releases/{ARCH}/autobuilds/latest-stage3.txt | grep -i systemd | grep -Ev "multi|desktop" | awk '"'{print $1}'"'', shell=True).decode('utf-8').strip()
+    stage3 = f"http://distfiles.gentoo.org/releases/{ARCH}/autobuilds/{tmp}"
+    os.system(f"curl -L -O {stage3}")
+    #os.system("mount -o bind /proc /mnt/proc")
+    os.system(f"tar --numeric-owner --xattrs -xvJpf stage3-*.tar.xz -C /mnt")
+    
+    ### STEP 2 STARTS HERE
