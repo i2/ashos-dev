732db37cef5a9bf1ef112ea90a58af51b766aba0
Reza <3228126+i2@users.noreply.github.com>
2022-07-26 17:24:08 -0300

    2022/07/26 - 17:24:08

--- src/prep/parted-mbr-example.sh
+++ src/prep/parted-mbr-example.sh
@@ -2,5 +2,5 @@
 
 sudo parted --align minimal --script $1 mklabel msdos unit MiB mkpart primary \
             ext4 1MiB 80% set 1 boot on mkpart primary ext4 80% 100%
-sudo mkfs.ext4 -L MBR ${1}1
+#sudo mkfs.ext4 -L MBR ${1}1
 
